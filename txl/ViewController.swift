9505c389b2c9d99eb025274d51133b614ebd5074
songzhiyuan <www.1319324030@qq.com>
2016-07-01 17:18:59 +0800

    通讯录
    本次试验要求做一个ios通讯录，经过今天的上机我只初步完成了通讯录的界面设置，具体代码还没有实现，还需完善很多东西。

---
+++ txl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  txl
+//
+//  Created by 20141105054 on 16/7/1.
+//  Copyright © 2016年 yuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
